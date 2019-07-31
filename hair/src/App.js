614aaad412c125f4f756e5547882000fe3481ed9
ebisLab <ebaiden@techie.com>
2019-07-31 17:50:31 -0400

    initial commit

---
+++ hair/src/App.js
@@ -0,0 +1,22 @@
+import React, {useState} from "react";
+//import logo from './logo.svg';
+//import Stylists from './components/Stylists'
+import './App.css';
+//import {profiles} from './components/data'
+//import { Route } from 'react-router-dom';
+//import InsideStylistDetails from './components/InsideStylistDetails'
+
+
+
+
+function App() {
+  //const [bringData, setBringData] = useState(profiles);
+
+  return (
+    <div className="App">
+     <h1>Stylists</h1>
+      </div>
+  );
+}
+
+export default App;
