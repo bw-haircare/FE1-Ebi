398fcb7677d52bad89df76aeed6115d9acc6854f
ebisLab <ebaiden@techie.com>
2021-01-07 19:15:27 -0500

    search profession

--- hair/src/components/Home.js
+++ hair/src/components/Home.js
@@ -21,6 +21,8 @@ import {
 } from "./styledComponents";
 
 function Home({bringData,stylists, isLoading, loggingIn, clients, fetchAllUsers}) {
+  const[results, setResult]=useState([])
+  const [dropDownTerm, setDropDownTerm]=useState("")
 
 
   useEffect(() => {
@@ -31,6 +33,28 @@ function Home({bringData,stylists, isLoading, loggingIn, clients, fetchAllUsers}
 
   console.log("LOADDIN", isLoading)
 
+  const searchResults=(e)=>{
+    e.preventDefault()
+
+    // setResult([...new Set(stylists.map(job=>job.profession))])
+    console.log("SEARCHINGGGGGG 💃🏻 💃🏻 💃🏻 💃🏻")
+    console.log("drop down term", dropDownTerm)
+    // setResult(stylists.map(job=>job.profession))
+    // const results = stylists.filter(person => person.toLowerCase().includes(searchTerm.toLowerCase()));
+    const profession_name=stylists.map(job=>job)
+    const result = profession_name.filter(person=>person.profession===dropDownTerm);
+    setResult(result)
+
+    console.log("results--->", result)
+
+  }
+
+  const changeHandler=(e)=>{
+  console.log("changing", e.target.name)
+  console.log("change 2", { [e.target.name]: e.target.value })
+  setDropDownTerm(e.target.value)
+  }
+
 
   return (
     <div>
@@ -38,13 +62,49 @@ function Home({bringData,stylists, isLoading, loggingIn, clients, fetchAllUsers}
 
       <H1>BROWSE AND DISCOVER BEAUTY PROFESSIONALS NEARBY</H1>
       <div className="StyleContainer ">
-        <div style={{display:"flex", flexWrap:"wrap"}}>
+          <form
+          onSubmit={searchResults}
+          style={{width:"100%", justifyContent:"center", display:"inline-flex", margin:"20px"}}
+          >
+        <input 
+        style={{width:"100%", height:"50px", margin:"10px", maxWidth:"900px"}}
+        />
+        <select name="job"  
+        id="jobs"
+                style={{width:"50%", height:"50px", margin:"10px", padding:"10px", maxWidth:"900px"}}
+                onChange={changeHandler}
+        >
+          {[...new Set(stylists.map(job=>job.profession))].map(item=><option value={`${item}`}>{item}</option>)}
+</select>
+
+
+        <button
+         style={{height:"50px", margin: "10px", width:"200px",
+        marginTop: "12px",
+        marginLeft: "12px",
+        boxShadow: "3px 8px",
+        border: "1px solid black",
+        boxShadow: "5px 10px",
+        padding: "5px",
+        verticalAlign: "super",
+        color:"black",
+        background:"white"
+        }}>Search</button>
+        </form>
+        
+        <div style={{display:"flex", flexWrap:"wrap"}}>   
 
 {isLoading===true ? (<div className="loadercontainer" style={{marginTop:"100%"}}>
   <div class="lds-ripple"><div></div><div></div></div>
-  </div>) : (stylists.map((user, i) => {
+  </div>) : (results.length==0?
+   (stylists.map((user, i) => {
           return <StylistDetails clients={clients} key={i} user={user} />;
-        }))}
+        }))
+  :
+  (results.map((user, i) => {
+    return <StylistSearchResult clients={clients} key={i} user={user} />;
+  }))
+  )}
 
 
 
@@ -60,6 +120,44 @@ function Home({bringData,stylists, isLoading, loggingIn, clients, fetchAllUsers}
 }
 
 function StylistDetails({user, clients}) {
+
+  return (
+    <Wrap>
+      <Link
+      clients={clients}
+        to={{
+          pathname: `/stylists/${user.id}`,
+          state: { ...user }
+        }}
+      >
+        <div className="left">
+          <CropImg>
+            <img alt={`${user.first}-${user.last}`}src={user.imgUrl} height="200px" />
+          </CropImg>{" "}
+        </div>
+        <div className="right">
+          <h2>
+            {" "}
+            {user.first} {user.last}{" "}
+          </h2>
+          <h3>{user.profession}</h3>
+          <p className="city">{user.location}</p>
+          <div className="stars">
+            <StarRatingComponent
+              name="rate1"
+              starCount={5}
+              starColor="pink"
+              renderStarIcon={() => <span>♥</span>}
+              value={2}
+            />
+          </div>
+        </div>
+      </Link>
+    </Wrap>
+  );
+}
+
+function StylistSearchResult({user, clients}) {
 
   return (
     <Wrap>
